61098bba5bc5df4733dea59bf252cec60cba5565
Sk-Muhaiminul-Hasan <shaikhmuhaiminulhasan@gmail.com>
2025-07-23 21:39:41 +0600

    The hero section and navbar

--- pawfect_match_client/src/Pages/Home/Home/Home.jsx
+++ pawfect_match_client/src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import Navbar from '../../../Components/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
             {/* Home page sections */}
+            <Navbar></Navbar>
             <Hero></Hero>
         </div>
     );
